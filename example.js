4ea28cf4df1e8a0942de6a808f930be445c6a95a
CarolSum <905965287@qq.com>
2018-11-10 16:04:21 +0800

    implement middleware & handle error

--- example.js
+++ example.js
@@ -1,10 +1,32 @@
 let Koa = require('./lib/application');
 let app = new Koa();
-app.use((req, res) => {
-    res.writeHead(200);
-    res.end('hello world');
+
+// app.use((ctx, next) => {
+//   ctx.res.writeHead(200);
+//   ctx.res.end('hello world');
+// });
+
+app.use(async (ctx, next) => {
+  console.log(1);
+  await next();
+  console.log(6);
+});
+
+app.use(async (ctx, next) => {
+  console.log(2);
+  await next();
+  console.log(5);
+});
+
+app.use(async (ctx, next) => {
+  console.log(3);
+  throw new Error('something err!');
+  ctx.body = 'hello world!';
+  ctx.res.writeHead(200);
+  ctx.res.end('hello world!');
+  console.log(4);
 });
 
 app.listen(3000, ()=>{
-    console.log('listening on 3000');
+  console.log('listening on 3000');
 })
